b86d62f2058d575dddef3e7ace0f55cd3196e89b
kcoveinon <kcove.inon@gmail.com>
2015-03-11 13:03:32 +0800

    #added route::group prefix

--- app/shared_classes/suppliers/HZ.php
+++ app/shared_classes/suppliers/HZ.php
@@ -88,7 +88,7 @@ class HZ extends SupplierApi
 	 */
 	public function getLocationDepots($locationCode, $countryCode)
 	{
-		set_time_limit(0);
+		ini_set('max_execution_time', 120);
 		$curlOptions = $this->defaultCurlOptions;
 		$xmlRequest = $this->getXmlForGetLocationDepots(
 								$locationCode,
@@ -111,7 +111,7 @@ class HZ extends SupplierApi
 	 */
 	public function getDepotDetails($locationCode, $countryCode)
 	{
-		set_time_limit(0);
+		ini_set('max_execution_time', 120);
 		$curlOptions = $this->defaultCurlOptions;
 		$xmlRequest = $this->getXmlForDepotDetails(
 								$locationCode,
