2e0da2450e47ef48891cf31d80323cf757b1ab47
kcoveinon <kcove.inon@gmail.com>
2015-03-16 07:00:29 +0800

    #added prodUrl

--- app/config/local/HZ.php
+++ app/config/local/HZ.php
@@ -1,10 +1,11 @@
 <?php
 
 $testUrl =  "https://vv.xqual.hertz.com/DirectLinkWEB/handlers/DirectLinkHandler?id=ota2007a";
+$prodUrl = "https://vv.xnet.hertz.com/DirectLinkWEB/handlers/DirectLinkHandler?id=ota2007a";
 
 return array(
 	'api' => array(
-		'url'              => $testUrl,
+		'url'              => $prodUrl,
 		'validationCode'         => 'B2S19P16R18',
 		'validationNumber'         => 'T487',
 		'consumerProductCode'         => 'A9CF',
